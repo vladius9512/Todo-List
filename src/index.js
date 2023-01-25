76b8d2715ffb50e82b64ba73821105d39f28cfe1
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-01-25 14:37:34 +0200

    Added 1 more button, fixed some more styling and created the basic classes in JS

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function Task(title, description, date, priority) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+}
+
+function Project(title) {
+    this.title = title;
+}
+
+let today = {};
+let thisWeek = {};
