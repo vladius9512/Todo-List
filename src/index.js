57e1b7665f84ba5619203d52b47043632872f2b9
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-01-25 21:07:15 +0200

    AddTaskBtn is now working, it has a visual and the form divs pops. Clicking away also makes it dissapear

--- src/index.js
+++ src/index.js
@@ -11,3 +11,21 @@ function Project(title) {
 
 let today = {};
 let thisWeek = {};
+
+const addTaskBtn = document.getElementById("addTaskBtn");
+const overlay = document.getElementById("overlay");
+const addTaskForm = document.getElementById("addTaskForm");
+
+addTaskBtn.addEventListener("click", (e) => {
+    const target = e.target;
+    overlay.classList.add("active");
+    addTaskForm.classList.add("active");
+});
+
+overlay.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target === overlay) {
+        addTaskForm.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+});
